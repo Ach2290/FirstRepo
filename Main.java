81fbc01d714eac6e17643c2defd42acf9656efa6
Alvaro <chaparro.garcia.alvaro@gmail.com>
2023-10-09 09:30:36 +0200

    Main prints farewell

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 .public static void Main(String[]args){
 	String solutation ="Hola Mundo";
 	System.outprintln(salutation);
+	String farewell ="Adios con el corazon";
+	System.outprintln(farawell)
 }
